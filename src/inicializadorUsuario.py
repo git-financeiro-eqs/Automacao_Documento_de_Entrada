7f5c6fed0e51bd9bd3ee6aa17c4ce05eab3e3b5c
Git_Financeiro_EQS <jesse.silva@eqsengenharia.com.br>
2025-03-19 10:11:36 -0300

    Update inicializadorUsuario.py

--- src/inicializadorUsuario.py
+++ src/inicializadorUsuario.py
@@ -1,4 +1,4 @@
-from pyautogui import hotkey, FAILSAFE, FailSafeException
+from pyautogui import press, hotkey, FAILSAFE, FailSafeException
 from time import sleep
 from selenium import webdriver                         
 from webdriver_manager.chrome import ChromeDriverManager
@@ -26,7 +26,7 @@ def inicializar_usuario():
     sleep(2)
     try:
         usuario = driver.find_element(By.XPATH, '/html/body/app-root/app-login/po-page-login/po-page-background/div/div/div[2]/div/form/div/div[1]/div[1]/po-login/po-field-container/div/div[2]/input')
-        usuario.send_keys("bot.contabil")
+        usuario.send_keys("**********")
     except:
         driver.quit()
         sleep(1)
@@ -39,6 +39,9 @@ def inicializar_usuario():
     sleep(2)
     hotkey("alt", "tab", interval=0.1)
     hotkey("alt", "tab", interval=0.1)
+    press(["tab"]*5)
+    sleep(0.5)
+    press("enter")
     while True:
         try:
             abriu = driver.find_element(By.XPATH, '/html/body/app-root/app-main/div/po-toolbar/div/div[2]/po-toolbar-notification/div/po-icon')
@@ -46,5 +49,3 @@ def inicializar_usuario():
         except:
             sleep(1)
     utils.checar_failsafe()
-
-   
